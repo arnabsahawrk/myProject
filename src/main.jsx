06a44e89ace5b70888e983b8ae1b335298cd31d7
arnabw <arnabsahawrk@gmail.com>
2024-05-05 19:23:44 +0600

    Added MUI and sync with tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes";
 import { HelmetProvider } from "react-helmet-async";
+import { StyledEngineProvider } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <StyledEngineProvider injectFirst>
+        <RouterProvider router={router} />
+      </StyledEngineProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
